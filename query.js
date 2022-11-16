84044d5435dd71488cb31dc32bdc0d67b5af14f5
Truong Pham <thanhtruong27701@gmail.com>
2022-11-16 17:45:11 +0700

    fix bug connect & query

--- query.js
+++ query.js
@@ -1,16 +1,29 @@
-const client = require("./connecting");
+const pg = require("pg");
+const myLogger = require("/Savage/indexing_nfts/logging/logger.js");
+require('dotenv').config();
+
+const pool = new pg.Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  port: process.env.PORT,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
 
 function excuteQuery(query) {
-  client.connect()
-
-  client.query(query, (err, res) => {
-    if (!err) {
-        console.log(res.rows);
-    } else {
-        console.log("Error occur: " + err.message)
-    }
-    client.end
-    })
+    pool.connect(function(err, client, done) {
+        if(err) {
+            myLogger.err('connection error', err)
+            return console.error('connection error', err);
+        }
+        client.query(query, function(err, result) {
+            done();
+            if(err) {
+                myLogger.err('error running query', err)
+                return console.error('error running query', err);
+            }
+        });
+    });
 }
 
 module.exports = {excuteQuery};
