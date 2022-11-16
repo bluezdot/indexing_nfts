8dd5d54771f8895c7e0e6312449b10a5efb28ea4
Truong Pham <thanhtruong27701@gmail.com>
2022-11-16 08:07:50 +0700

    update module

---
+++ typeConvert.js
@@ -0,0 +1,11 @@
+export function hex_to_address(raw_hex) {
+  const clean_hex = raw_hex.substring(26,);
+  const address = '0x'.concat(clean_hex);
+  return address;
+}
+
+export function hex_to_decimal(raw_hex) {
+  const clean_hex = raw_hex.substring(2,);
+  const decimal = parseInt(clean_hex, 16);
+  return decimal
+}
