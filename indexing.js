c18594d6a0a7ba461799c8d97e2902f098216840
Truong Pham <thanhtruong27701@gmail.com>
2022-11-16 08:06:36 +0700

    clean address

--- indexing.js
+++ indexing.js
@@ -1,9 +1,32 @@
 const { ethers } = require("ethers");
+const { client } = require("./connecting.js");
+const express = require("express");
+const app = express();
+
+// openning a port
+app.listen(3000, ()=> {
+    console.log("Server is listening on port 3000");
+})
+
+// convert raw_hex to address/NFTs ID
+function hex_to_address(raw_hex) {
+    const clean_hex = raw_hex.substring(26,);
+    const address = '0x'.concat(clean_hex);
+    return address;
+}
+  
+function hex_to_decimal(raw_hex) {
+    const clean_hex = raw_hex.substring(2,);
+    const decimal = parseInt(clean_hex, 16);
+    return decimal
+}
 
 async function main() {
 
     console.log("-- START --");
 
+    const start = 23045358;
+    const end = 23050358;
     // var url = "https://1rpc.io/bnb";
     var url = "https://binance.nodereal.io";
     const provider = new ethers.providers.JsonRpcProvider(url);
@@ -19,16 +42,28 @@ async function main() {
     const knightContract = new ethers.Contract(knightAddress, knightAbi, provider);
 
     // QUERY STATEMENT //
-    blockNumber = await provider.getBlockNumber();
+    // blockNumber = await provider.getBlockNumber();
     // balance = await provider.getBalance("0x6e143f784Cb160f10966096AB2aC4207D633d5d7");
     filter = knightContract.filters.Transfer(null, null); // filter all addresses to all addresses
-    query = await knightContract.queryFilter(filter, 23072034, 23077034);
+    ev_query = await knightContract.queryFilter(filter, start, end);
 
     // LOGGING //
-    console.log(blockNumber);
-    console.log(ethers.utils.formatEther(balance));
-    console.log(query[1]);
-    console.log(Object.keys(query).length);
+    // console.log(blockNumber);
+    // console.log(ethers.utils.formatEther(balance));
+    console.log(ev_query);
+    console.log(`number of transfer from ${start} to ${end}: ${Object.keys(ev_query).length}`);
+
+    // connecting to database
+    // client.connect()
+    // db_query = "CREATE TABLE test (id int, name varchar, age int)"
+    // client.query(db_query, (err, res) => {
+    // if (!err) {
+    //     console.log(res.rows);
+    // } else {
+    //     console.log("Error occur: " + err.message)
+    // }
+    // client.end
+    // })
 
     console.log("-- END --");
 
