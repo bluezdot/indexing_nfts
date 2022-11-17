d9449f1fe699fb77a065a95ad4bd737859921055
Truong Pham <thanhtruong27701@gmail.com>
2022-11-17 11:09:13 +0700

    update log

--- logging/logger.js
+++ logging/logger.js
@@ -1,8 +1,9 @@
 const { Console } = require("console");
 const fs = require("fs");
+require('dotenv').config();
 
 const myLogger = new Console({
-  stdout: fs.createWriteStream("./logging/norLog.txt"),
+  stdout: fs.createWriteStream(process.env.LOG_PATH),
   stderr: fs.createWriteStream("./logging/errLog.txt"),
 });
 
