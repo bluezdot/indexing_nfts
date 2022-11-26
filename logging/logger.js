5c7bc378dc4276f6d15e19db99620daaf171dd9b
Truong Pham <thanhtruong27701@gmail.com>
2022-11-26 09:39:03 +0700

    refactor faraland/aspo

--- logging/logger.js
+++ logging/logger.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 require('dotenv').config();
 
 const myLogger = new Console({
-  stdout: fs.createWriteStream(process.env.LOG_PATH),
+  stdout: fs.createWriteStream(process.env.KNIGHT_LOG_PATH),
   stderr: fs.createWriteStream("./logging/errLog.txt"),
 });
 
